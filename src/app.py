0dcafbe3f7c6edff7379b04233cd1869615ef8fb
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2022-11-03 22:38:59 +0600

    Format the plot

--- src/app.py
+++ src/app.py
@@ -1,13 +1,11 @@
 from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
-import pandas as pd
 import yfinance as yf
-
 
 app = Dash(__name__)
 
 
-period_options = [
+period_choices = [
     "6mo",
     "1y",
     "2y",
@@ -17,14 +15,15 @@ period_options = [
     "max",
 ]
 
+stock_choices = ['TSLA', 'GOOGL', 'AAPL', 'AMZN']
 
 app.layout = html.Div(
     children=[
         html.H1(children='Financial Advisor'),
-        dcc.RadioItems(period_options, period_options[0], id="period"),
+        dcc.RadioItems(period_choices, period_choices[0], id="period"),
         dcc.Input(id="investment-amount", type='number'),
         dcc.Dropdown(
-            ['TSLA', 'GOOGL', 'AAPL', 'AMZN'],
+            stock_choices,
             ['TSLA', 'GOOGL'],
             multi=True,
             id="stocks",
@@ -41,12 +40,28 @@ app.layout = html.Div(
     Input('stocks', 'value'),
 )
 def update_output_div(period, investment_amount, selected_stocks):
-    df = (
-        yf.download(selected_stocks, period=period)[['Close']]
-        .droplevel(0, axis=1)
-        .reset_index()
+    try:
+        df = (
+            yf.download(selected_stocks, period=period)[['Close']]
+            .droplevel(0, axis=1)
+            .reset_index()
+        )
+    except ValueError:
+        df = yf.download(selected_stocks, period=period)[['Close']].reset_index()
+
+    df.iloc[:, 1:] = ((df.iloc[:, 1:].pct_change(1) + 1).cumprod() - 1).fillna(0)
+
+    fig = px.line(
+        df,
+        x='Date',
+        y=df.columns[1:],
+        template="simple_white",
     )
-    return px.line(df, x='Date', y=df.columns[1:])
+    fig.update_traces(hovertemplate='%{y:.1%}')
+    fig.update_layout(yaxis_tickformat=".0%")
+    fig.update_layout(hovermode="x unified")
+
+    return fig
 
 
 if __name__ == '__main__':
