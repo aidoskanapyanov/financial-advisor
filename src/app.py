ee26556845c17739d8033519d83fd59ccf14d897
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2022-11-04 15:51:00 +0600

    Host on 0

--- src/app.py
+++ src/app.py
@@ -211,4 +211,4 @@ def update_output_div(period, investment_amount, selected_stocks, btn):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0')
